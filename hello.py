0afca4f82908cfc91fe88cce2beebe3462a3b987
Rodgers-M <mkishagha@gmail.com>
2016-10-15 13:06:39 +0300

    fetching a Json object

--- hello.py
+++ hello.py
@@ -1,10 +1,17 @@
-from Flask import Flask
+from flask import Flask
+from flask import jsonify, make_response
+app = Flask('rodgy-library')
 
-app = Flask('rodgy')
+books = {
+	'name': 'Harry Potter and the priosoner',
+	'auther': 'jk. Rowling',
+	'category': 'Magic/Fiction',
+	'id': '4325345234'
+}
 
-@app.route('/api/categoty/books', methods=['GET', 'POST'])
+@app.route('/api/category/books/', methods=['GET', 'POST'])
 def book_api():
-	return 'some resource'
+	return jsonify(books)
 
 if __name__ == '__main__':
 	app.run()
